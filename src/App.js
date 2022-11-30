bcb7afd75120cc9498c54a790266ff36aa5e4c9d
JonathanDev <jonathan.cardozo.dev@gmail.com>
2022-11-30 17:01:38 -0500

    explicando useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 
+const GIFS = [
+  "https://media3.giphy.com/media/EatwJZRUIv41G/giphy.gif?cid=ade247d8pqnen0j7wwy847xq7l0p8gc6a5i1h91uxdoijfka&rid=giphy.gif&ct=g",
+  "https://media3.giphy.com/media/EPcvhM28ER9XW/giphy.gif?cid=ade247d8pqnen0j7wwy847xq7l0p8gc6a5i1h91uxdoijfka&rid=giphy.gif&ct=g"
+]
+
+const DIFFERENT_GIFTS = [
+  "https://media4.giphy.com/media/cnbsOTkEJnq0/giphy.gif?cid=ecf05e478w1ki0s965jz0n7jmdtwp3bkkdrvm9qab6xertbv&rid=giphy.gif&ct=g",
+]
 
 function App() {
-  const [value, updateValue] = useState(["https://media2.giphy.com/media/8nbh0rkQwd5T2/giphy.gif?cid=ade247d8zxnsfcdqmi7kqhgddyjenei8xmrncqixm735yog1&rid=giphy.gif&ct=g"]) // es lo mismo que tenemos abajo
-  // const state = useState([])
-  // const value = state[0]
-  // const updateValue = state[1]
+  const [gifs, setGifs] = useState(GIFS)
+
+  // Recibe dos parametros la funcion a ejecutar y las dependencias que tiene este efecto
+  // en este caso un array vacio que indica cuantas veces se va a renderizar evitando loop
+  // que cada vez que se actualize el estado ejecute el efecto y el efecto ejecuta una actualizacion del estado
+  useEffect(function () {
+    console.log(' efecto ejecutado');
+    setGifs(DIFFERENT_GIFTS)
+  },[])
 
   return (
     <div className="App">
       <section className="App-content">
-        <img src={value} alt="" srcset="" />
+        {
+          gifs.map(singleGif => <img src={singleGif}/> )
+        }
+        {/* Asi escuchamos eventos */}
+      <button onClick={() => setGifs(DIFFERENT_GIFTS)}>
+        Cambiar giff
+      </button>
+        {/* <img src={gifs} alt="" srcset="" /> */}
       </section>
     </div>
   );
