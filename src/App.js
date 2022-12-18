ff8c584735a31963e39f7878de2526953d5b4d65
JonathanDev <jonathan.cardozo.dev@gmail.com>
2022-12-18 00:25:42 -0500

    Las dependencias de useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import ListOfGifs from "./components/ListOfGifs";
 
 function App() {
+  const [keyword, setKeyword] = useState("GTA");
   return (
     <div className="App">
       <section className="App-content">
-        <ListOfGifs keyword="GTA" />
+        {/* y para evitar caer en un loop infinito este no puede regresarnos al estado anterior */}
+        <button onClick={() => setKeyword("mapache")}>Cambiar Keyword</button>
+        {/* Tenemos en la aplicacion el componente ListOfGifs al que le pasamos una prop keyword para hacer la llamada a la API */}
+        <ListOfGifs keyword={keyword} />
       </section>
     </div>
   );
--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -2,12 +2,19 @@ import React, { useEffect, useState } from "react";
 import Gifs from "./Gifs";
 import getGifs from "../services/getGifs";
 
-export default function ListOfGifs({ keywords }) {
+// En vez de enviar los gifs Pasamos la palabra clave que queremos usar para buscar
+export default function ListOfGifs({ keyword }) {
+  // Llega la prop keyword, hay un estado incial que es un array vacio sin resultados al principio
   const [gifs, setGifs] = useState([]);
 
-  useEffect(function () {
-    getGifs({ keywords }).then((gifs) => setGifs(gifs));
-  }, []);
+  // Luego hacemos un efecto de forma que la primera vez que se renderize haremos una llamada a la API con esa keyword que pasamos por parametro
+  useEffect(
+    function () {
+      // Esta keyword es una dependencia del efecto
+      getGifs({ keyword }).then((gifs) => setGifs(gifs));
+    },
+    [keyword],
+  );
 
   return gifs.map(({ id, title, url }) => (
     // Warning: Each child in a list should have a unique "key" prop
