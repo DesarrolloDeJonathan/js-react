6daf849b79947c926b2ed7991c18b06af8f26794
JonathanDev <jonathan.cardozo.dev@gmail.com>
2022-12-24 10:40:49 -0500

    Guardando un objeto en el State del componente

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -4,27 +4,33 @@ import getGifs from "../services/getGifs";
 
 export default function ListOfGifs({ params }) {
   const { keyword } = params;
-  const [loading, setLoading] = useState([]);
-  const [gifs, setGifs] = useState([]);
+  // const [loading, setLoading] = useState([]);
+  // En lugar de tener un array tendremos un objeto que tenga dos llaves
+  const [gifs, setGifs] = useState({ loading: false, results: [] });
 
   useEffect(
     function () {
-      setLoading(true);
+      // setLoading(true);
+      setGifs({ loading: true, results: gifs.results });
       getGifs({ keyword }).then((gifs) => {
-        setGifs(gifs);
-        setLoading(false);
+        setGifs({ loading: false, results: gifs });
+
+        // setGifs(gifs);
+        // setLoading(false);
       });
     },
-    [keyword],
+    // Este gifs.results lo necesitaremos como dependencia del efecto
+    // Pero al hacerlo generamos que el resultado siempre cambie y genera un bucle (se pone loco)
+    [gifs.results, keyword],
   );
 
-  if (loading) return <i>Cargando 🥳</i>;
+  if (gifs.loading) return <i>Cargando 🥳</i>;
 
   // React no trata tan bien a los arreglos asi que lo envolvemos dentro de un div
   // ya que funciona mejor con componentes
   return (
     <div>
-      {gifs.map(({ id, title, url }) => (
+      {gifs.results.map(({ id, title, url }) => (
         <Gifs key={id} id={id} title={url} url={url} />
       ))}
     </div>
